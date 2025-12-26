46c9ee83c7c46a0b91e329945d4d61d3c1fc4942
antonishche <antonalfer2008@gmail.com>
2025-12-26 14:47:13 +0300

    rgr3 part1

--- semester_1/rgr3_set_list_stl/part1/main.cpp
+++ semester_1/rgr3_set_list_stl/part1/main.cpp
@@ -1,9 +1,75 @@
+#include <iostream>
+#include <limits>
+#include <set>
+#include <stdexcept>
 
+size_t getN() {
+  long long input;
 
+  std::cout << "Введите N: ";
+
+  if (!(std::cin >> input)) {
+    throw std::invalid_argument("введено некорректное значение или число слишком велико.");
+  }
+
+  if (input <= 0) {
+    throw std::out_of_range("число N должно быть положительным.");
+  }
+
+  if (input > 100000) {
+    throw std::out_of_range("слишком большое N.");
+  }
+
+  return static_cast<size_t>(input);
+}
+
+void fillNums(std::set<size_t>& nums, size_t N) {
+  for (size_t i = 2; i <= N; i++) {
+    nums.insert(i);
+  }
+}
+
+void filterNums(std::set<size_t>& nums, size_t N) {
+  for (size_t num = 2; num * num <= N; num++) {
+    if (nums.count(num)) {
+      for (size_t toDelete = num * num; toDelete <= N; toDelete += num) {
+        nums.erase(toDelete);
+      }
+    }
+  }
+}
+
+void printNums(const std::set<size_t>& nums, size_t N) {
+  std::cout << "*\nВсе простые числа, не превосходящие " << N << ":\n";
+
+  if (nums.empty()) {
+    std::cout << "Отсутствуют.\n";
+  } else {
+    for (const size_t num : nums) {
+      std::cout << num << " ";
+    }
+  }
+}
 
 int main() {
+  // your code goes here
 
-    // your code goes here
+  setlocale(LC_ALL, "Russian");
 
-    return 0;
+  try {
+    size_t N = getN();
+
+    std::set<size_t> nums;
+
+    fillNums(nums, N);
+    filterNums(nums, N);
+    printNums(nums, N);
+
+  } catch (const std::exception& e) {
+    std::cerr << "Ошибка: " << e.what() << std::endl;
+  } catch (...) {
+    std::cerr << "Произошла неизвестная ошибка." << std::endl;
+  }
+
+  return 0;
 }
